bc6b71e8376f6c98ed3eaf7a7e128731f502a988
myuser <myemail>
2024-03-11 22:39:29 -0400

    project starting

---
+++ src/container/Home/index.jsx
@@ -0,0 +1,18 @@
+import {Container, ButtonContainer} from  "./styles";
+
+export const Home = () => {
+    return (
+        <Container>
+            <p>TiagoPdaS Sudoku Game</p>
+            <table>
+                <tbody>
+                    <tr>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <ButtonContainer></ButtonContainer>
+        </Container>
+    )
+}
